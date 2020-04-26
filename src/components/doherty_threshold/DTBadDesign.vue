3e88dc76248478294e804206bf2c53ddeecd5b7f
david <david.daradics@synetiq.net>
2020-04-26 15:02:49 +0200

    Fixes

--- src/components/doherty_threshold/DTBadDesign.vue
+++ src/components/doherty_threshold/DTBadDesign.vue
@@ -4,7 +4,7 @@
             <v-col cols="4">
                 <v-list flat>
                     <v-list-item
-                            @click="selectItem"
+                            @click="selectItem(1)"
                     >
                         <v-list-item-content>
                             <v-list-item-title>
@@ -13,7 +13,7 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item
-                            @click="selectItem"
+                            @click="selectItem(2)"
                     >
                         <v-list-item-content>
                             <v-list-item-title>
@@ -25,8 +25,45 @@
             </v-col>
             <v-col cols="8">
                 <v-card
-                        id="card"
-                        class="mx-auto"
+                        id="card1"
+                        class="mx-auto cardItem"
+                        max-width="400"
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="   https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                    >
+                        <v-card-title>Houses to not buy</v-card-title>
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">House</v-card-subtitle>
+
+                    <v-card-text class="text--primary">
+                        <div>Big house</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="orange"
+                                text
+                        >
+                            Share
+                        </v-btn>
+
+                        <v-btn
+                                color="orange"
+                                text
+                        >
+                            Explore
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+
+
+                <v-card
+                        id="card2"
+                        class="mx-auto cardItem"
                         max-width="400"
                 >
                     <v-img
@@ -78,13 +115,27 @@
 <script>
     export default {
         name: 'DTBadDesign',
+        mounted() {
+            const cards = document.getElementsByClassName('cardItem')
+            console.log(cards)
+            for (const card of cards) {
+                card.style.display = 'none'
+            }
+        },
         methods: {
-            selectItem: async function () {
+            selectItem: async function (index) {
+                const cards = document.getElementsByClassName('cardItem')
+
+
+                for (const card of cards) {
+                    card.style.display = 'none'
+                }
+
                 const wait = Math.random() * (2000 - 800) + 800;
 
-                document.getElementById('card').style.display = 'none'
+                document.getElementById('card' + index).style.display = 'none'
                 await new Promise(r => setTimeout(r, wait));
-                document.getElementById('card').style.display = 'block'
+                document.getElementById('card' + index).style.display = 'block'
             }
         }
     }
--- src/components/doherty_threshold/DTGoodDesign.vue
+++ src/components/doherty_threshold/DTGoodDesign.vue
@@ -4,7 +4,7 @@
             <v-col cols="4">
                 <v-list flat>
                     <v-list-item
-                            @click="selectItem"
+                            @click="selectItem(1)"
                     >
                         <v-list-item-content>
                             <v-list-item-title>
@@ -13,7 +13,7 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item
-                            @click="selectItem"
+                            @click="selectItem(2)"
                     >
                         <v-list-item-content>
                             <v-list-item-title>
@@ -31,44 +31,85 @@
                         type="card"
                 ></v-skeleton-loader>
 
-                <v-card
-                        v-else
-                        id="card"
-                        class="mx-auto"
-                        max-width="400"
-                >
-                    <v-img
-                            class="white--text align-end"
-                            height="200px"
-                            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+
+                <template v-else>
+
+                    <v-card
+                            v-if="visible===1"
+                            class="mx-auto cardItem"
+                            max-width="400"
                     >
-                        <v-card-title>Top 10 Australian beaches</v-card-title>
-                    </v-img>
-
-                    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-                    <v-card-text class="text--primary">
-                        <div>Whitehaven Beach</div>
-
-                        <div>Whitsunday Island, Whitsunday Islands</div>
-                    </v-card-text>
-
-                    <v-card-actions>
-                        <v-btn
-                                color="orange"
-                                text
+                        <v-img
+                                class="white--text align-end"
+                                height="200px"
+                                src="   https://cdn.vuetifyjs.com/images/cards/house.jpg"
                         >
-                            Share
-                        </v-btn>
+                            <v-card-title>Houses to not buy</v-card-title>
+                        </v-img>
 
-                        <v-btn
-                                color="orange"
-                                text
+                        <v-card-subtitle class="pb-0">House</v-card-subtitle>
+
+                        <v-card-text class="text--primary">
+                            <div>Big house</div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-btn
+                                    color="orange"
+                                    text
+                            >
+                                Share
+                            </v-btn>
+
+                            <v-btn
+                                    color="orange"
+                                    text
+                            >
+                                Explore
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+
+
+                    <v-card
+                            v-if="visible===2"
+                            class="mx-auto cardItem"
+                            max-width="400"
+                    >
+                        <v-img
+                                class="white--text align-end"
+                                height="200px"
+                                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                         >
-                            Explore
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
+                            <v-card-title>Top 10 Australian beaches</v-card-title>
+                        </v-img>
+
+                        <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+                        <v-card-text class="text--primary">
+                            <div>Whitehaven Beach</div>
+
+                            <div>Whitsunday Island, Whitsunday Islands</div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-btn
+                                    color="orange"
+                                    text
+                            >
+                                Share
+                            </v-btn>
+
+                            <v-btn
+                                    color="orange"
+                                    text
+                            >
+                                Explore
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+
+                </template>
             </v-col>
             <v-btn
                     fab
@@ -88,14 +129,17 @@
         name: 'DTGoodDesign',
         data: function () {
             return {
-                loading: false
+                loading: false,
+                visible: undefined
             }
         },
         methods: {
-            selectItem: async function () {
+            selectItem: async function (index) {
+                this.visible = index
                 this.loading = true
                 await new Promise(r => setTimeout(r, 350));
                 this.loading = false
+
 
             }
         }
